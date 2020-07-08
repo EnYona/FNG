1443ece5caef2f28f81beb5b6c738d88deeea508
EnYona <67996044+EnYona@users.noreply.github.com>
2020-07-08 13:13:15 +0700

    Add files via upload

---
+++ FNG.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+#by EnYona
+
+clear
+purple='\e[0;35m'
+brown='\e[0;33m'
+lightgay='\e[0;37m'
+darkgray='\e[1;30m'
+lightblue='\e[1;34m'
+lightgreen='\e[1;32m'
+lightcyan='\e[1;36m'
+lightred='\e[1;31m'
+lightpurple='\e[1;35m'
+yellow='\e[1;33m'
+white='\e[1;37m'
+echo
+toilet -f big -F gay By FNG
+
+
+nama="ABCDFNG"
+until [ "$siapa" = "$nama" ]
+do
+   read -p "Masukan ID :" siapa
+done
+echo 'Plase Wait...'
+sleep 3
+echo 'Loading..'
+sleep 2
+echo "\033[1;37m[====="
+sleep 1
+echo "\033[1;32m[========"
+sleep 1
+echo "\033[1;35m[============"
+sleep 1
+echo "\033[1;33m[==============="
+sleep 1
+echo "\033[1;34m[==================="
+sleep 1
+echo "\033[1;30m[======================"
+sleep 1
+echo ""
+echo "\033[0;35m#############################################"
+echo "\033[0;37m============================================="
+echo "\033[1;37m"
+echo "( 1 ) Database FNG"
+echo "( 2 ) Misi Misi FNG"
+echo "( 3 ) Terbentuk Dan Tujuan FNG"
+echo "( 4 ) Bergabung FNG ? "
+echo "( 5 ) exit "
+echo "\033[0;37m=============================================="
+echo "\033[0;35m##############################################"
+echo "\033[1;37m"
+read -p '''Pilih Mana Saudaraku ?: ''' pilih;
+echo ""
+if [ $pilih = "1" ];
+then
+#kalian isi Database Team Kita
+	echo "\033[1;36m"
+	echo "SOLD"
+	sleep 1
+
+elif [ $pilih = "2" ];
+then
+#Bagi Para Admin Atau Mod Kalian Tulis Misi Selanjutnya
+	echo "\033[1;36m"
+	echo "SOLD"
+	sleep 1
+
+elif [ $pilih = "3" ];
+then
+#alwi lu boleh tulis kenapa terbentuknya FNG
+	echo "\033[1;36m"
+	echo "SOLD"
+	sleep 1
+
+elif [ $pilih = "4" ];
+then
+#Admin Kasi Tau Tu Syaratnya Apa Aja
+	echo "\033[1;36m"
+	echo "Chat Admin Atau MOD"
+	echo "+62 831-7142-2625(ADMIN)"
+	echo "+62 895-3248-57580(MOD)"
+	echo "+62 857-7684-9928(ADMIN)"
+	echo "+62 878-3383-8849(ADMIN)"
+	sleep 1
+#Untuk Para Pekerja Pilihan Exit Jangan Di Rubah!!!
+elif [ $pilih = "5" ];
+then
+	echo "\033[1;36m"
+	echo "EXIT"
+	echo "Selamat Beristirahat:)"
+	echo "\033[0;35m##########################"
+fi
+echo "\033[1;37m"
